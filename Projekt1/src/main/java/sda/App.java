b1df36d8cc97e1e8a820762f96bfa28f5863c29e
Rafal Woronowicz <woronowicz87@gmail.com>
2019-01-26 15:42:51 +0100

    mapy/slownik

--- Projekt1/src/main/java/sda/App.java
+++ Projekt1/src/main/java/sda/App.java
@@ -65,5 +65,35 @@ public class App
         System.out.println(mojaMapa.dictionary.get("piesek"));
         System.out.println(mojaMapa.dictionary.get("kotek"));
 
+        mojaMapa.modifyWord("piesek","daug");
+        mojaMapa.modifyWord("kociatko","cat");
+        System.out.println("piesek - "+mojaMapa.getWord("piesek"));
+        System.out.println(mojaMapa.dictionary.get("piesek"));
+        System.out.println(mojaMapa.dictionary.get("kociatko"));
+        System.out.println(mojaMapa.dictionary.get("piesek"));
+        mojaMapa.removeTrans("piesek");
+        System.out.println(mojaMapa.dictionary.get("piesek"));
+        mojaMapa.addWord("statek","ship");
+        System.out.println(mojaMapa.getFullList());
+
+        mojaMapa.addWord2("piesek","dog1","dog2");
+        System.out.println(mojaMapa.dictionary2);
+        System.out.println(mojaMapa.getWord2("piesek"));
+        System.out.println(mojaMapa.dictionary);
+        System.out.println("wyszukiwanie: ");
+        mojaMapa.addWord("piesek1","dog1");
+        mojaMapa.addWord("elopiesek1","dog2");
+        mojaMapa.addWord("hehespiesesek1","dog3");
+
+        System.out.println(mojaMapa.askSearch("ese"));
+
+
+
+
+
+
     }
+
+
+
 }
