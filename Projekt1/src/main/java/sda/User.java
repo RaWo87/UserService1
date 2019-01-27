1b409b395089849da07ca73951c0350696d90f1d
Rafal Woronowicz <woronowicz87@gmail.com>
2019-01-27 11:23:16 +0100

    rozwiniety user service

--- Projekt1/src/main/java/sda/User.java
+++ Projekt1/src/main/java/sda/User.java
@@ -1,10 +1,33 @@
 package sda;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.util.Date;
+
 public class User {
     private String imie;
     private String nazwisko;
     private String login;
     private String haslo;
+    private LocalDate dataUrodzenia;
+    private String emailAdress;
+
+
+
+    public void setEmail(String adres){
+        this.emailAdress = adres;
+    }
+    public String getEmail(){
+        return emailAdress;
+    }
+
+    public void setWiek(int year, int month, int day){
+        this.dataUrodzenia=LocalDate.of(year,month,day);
+    }
+    public int getAge(){
+        return Period.between(dataUrodzenia, LocalDate.now()).getYears();
+    }
 
     public void setImie(String imie){
         this.imie = imie;
@@ -45,6 +68,8 @@ public class User {
                 ", nazwisko='" + nazwisko + '\'' +
                 ", login='" + login + '\'' +
                 ", haslo='" + haslo + '\'' +
+                ", data ur='" + dataUrodzenia + '\'' +
                 '}';
     }
+
 }
--- Projekt1/src/main/java/sda/UserService.java
+++ Projekt1/src/main/java/sda/UserService.java
@@ -1,12 +1,81 @@
 package sda;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class UserService {
 
     private User[] uzytkownicy = new User[5];
     private int count = 0;
     private ArrayList<User> moiUserzy = new ArrayList<>();
+
+
+
+    private Map<Integer,User> mapaUserow = new HashMap<>();
+    private int userIDs = 1;
+
+    public void removeByName(String userName) {
+        for (User currentUser : mapaUserow.values()) {
+            if(currentUser.getImie().equals(userName)){
+            mapaUserow.remove(currentUser);
+            }
+            if(currentUser.getNazwisko().equals(userName)){
+                mapaUserow.remove(currentUser);
+            }
+
+
+            }
+    }
+
+    public void addToMap(User user) {
+        boolean brakduplikatów = true;
+        if (mapaUserow.isEmpty()) {
+            mapaUserow.put(this.userIDs, user);
+            userIDs++;
+        } else {
+            for (User currentUser : mapaUserow.values()) {
+                if (currentUser.getEmail().equals(user.getEmail())) {
+                    brakduplikatów=false;
+                }
+            }
+            if(brakduplikatów == true){
+                mapaUserow.put(this.userIDs, user);
+                userIDs++;
+            }
+        }
+    }
+
+    public User getUserByID(int id){
+
+        return mapaUserow.get(id);
+    }
+    public List<User> getYounger(){
+        List<User> tempList = new ArrayList<>();
+        for(User currentUser: mapaUserow.values()){
+            if(currentUser.getAge() <18){
+                tempList.add(currentUser);
+            }
+        }
+        return tempList;
+    }
+
+    public Map<String,Integer> getOlder(){
+        Map<String,Integer> tempMap = new HashMap<>();
+
+        for(User currentUser: mapaUserow.values()){
+            if(currentUser.getAge() >=18){
+                tempMap.put(currentUser.getImie(), currentUser.getAge());
+            }
+
+        }
+        return tempMap;
+    }
+
+
+
+
 
     public  void addUserL(User user){
         moiUserzy.add(user);
